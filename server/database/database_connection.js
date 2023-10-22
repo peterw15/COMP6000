d76e84a51bf80b34294a8f965fc6320eb78e97bc
Bm472 <bm472@kent.ac.uk>
2023-10-22 15:02:19 +0100

    MySQL Connectivity

--- server/database/database_connection.js
+++ server/database/database_connection.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql');
+var mysql2 = require('mysql2');
 
-var con = mysql.createConnection({
+var con = mysql2.createConnection({
     host: "dragon.kent.ac.uk",
     user: "comp6000_09",
     password: "p3oulla",
