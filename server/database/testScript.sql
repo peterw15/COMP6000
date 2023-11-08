51241a556e06e3c676a77446862443c7c9258858
pegw2 <peterw1502@gmail.com>
2023-11-08 19:48:56 +0000

    added a test user

---
+++ server/database/testScript.sql
@@ -0,0 +1,2 @@
+INSERT comp6000_09.User (UserID, firstName, lastName, email) VALUES ("1","TestFirstName","TestLastName","test@gmail.com");
+INSERT comp6000_09.Authentication (UserID, username, password) VALUES ("1","TestUserName","TestPassword");
