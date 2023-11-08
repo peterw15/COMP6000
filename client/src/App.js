34cbd38034233942589cf9167e9ca2ef07f0e4fd
pegw2 <peterw1502@gmail.com>
2023-11-08 15:24:58 +0000

    moved files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "./login-Page/LoginPage";
-import HomePage  from "./homepage/HomePage";
+import LoginPage from "./pages/login-Page/LoginPage";
+import HomePage  from "./pages/homepage/HomePage";
 
 function App() {
 
-    // To add a route: Create a new <Route />, Set the path to the URL Address, Set the element to the pages default function (Peter 07/11/23)   
+    // To add a route: Create a new <Route />, Set the path to the URL Address, Set the element to the pages default function. (Peter 07/11/23)   
 
     return (
     <Routes>
