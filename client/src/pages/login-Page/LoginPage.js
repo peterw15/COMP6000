cf18cfb238c2e688bd21173abaf00cdf7126a1b6
Bm472 <bm472@kent.ac.uk>
2023-11-09 00:32:14 +0000

    Implemented login validation by connecting front and backend

--- client/src/pages/login-Page/LoginPage.js
+++ client/src/pages/login-Page/LoginPage.js
@@ -1,22 +1,35 @@
 import './loginPageStyleSheet.css';
 import {Link} from "react-router-dom";
+import Axios from 'axios';
+
 
 function LoginPage() {
+
+    const handleClick = () => {
+        Axios.post('http://localhost:3001/login', {
+            username : document.getElementById("username").value,
+            password : document.getElementById("password").value
+        }).then(res => isRegistered(res.data)).catch(error => console.log(error));
+    }
+
+    const isRegistered = (registered) => registered ? console.log("VALIDATED") : console.log("NOT VALIDATED");
+ 
+
     return (
         <>
             <div className="loginPage">
                 <div className="header"> Login </div>
-                <div className="form">
-                    <h2 className="subHeader"> Username </h2>
-                    <input id="username" />
-                    <h2 className="subHeader"> Password</h2>
-                    <input id="password" /> <br />
-                    <button id="submit" className="btn"> Submit </button> <br /> <br /> <br />
-                    <button id="forgotPassword" className="btn"> Forgot Login </button>
-                    <Link to="/register" >
-                        <button id="createAccount" className="btn"> Create Account </button>
-                    </Link>
-                </div>
+                    <div className="form">
+                            <h2 className="subHeader"> Username </h2>
+                            <input id="username" />
+                            <h2 className="subHeader"> Password</h2>
+                            <input id="password" /> <br />
+                            <button id="submit" className="btn" onClick={handleClick}> Submit </button> <br /> <br /> <br />
+                            <button id="forgotPassword" className="btn"> Forgot Login </button>
+                            <Link to="/register" >
+                                <button id="createAccount" className="btn"> Create Account </button>
+                            </Link>
+                    </div>
             </div>
         </>
     );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,41 @@
 // server/index.js
 
 const express = require("express");
+const mysql2 = require('mysql2');
+const cors = require("cors");
+
+const con = mysql2.createConnection({
+    host: "dragon.kent.ac.uk",
+    user: "comp6000_09",
+    password: "p3oulla",
+    database: "comp6000_09"
+})
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.send("It's working!")
+})
+
+app.post('/login', (req,res) => {
+  console.log(req.body);
+  const body = req.body;
+  const results = con.query("SELECT * FROM Authentication WHERE username = ? AND password = SHA2(?,256)", [body.username, body.password], function (err, result, fields) {
+    if (err) throw err;
+    result.length == 0 ? res.send(false) : res.send(true);
+  });
+  console.log(results);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
