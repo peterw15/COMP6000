8bfebbb17b6f5ab979e78589c4dc6770e2c7826e
Luke <lukeelliottkent@gmail.com>
2023-11-18 00:32:17 +0000

    updated db with error checks / reformat

--- server/database/database_connection.js
+++ server/database/database_connection.js
@@ -1,5 +1,7 @@
 var mysql2 = require('mysql2');
 
+
+// connecting to database with login details
 var con = mysql2.createConnection({
     host: "dragon.kent.ac.uk",
     user: "comp6000_09",
@@ -7,11 +9,36 @@ var con = mysql2.createConnection({
     database: "comp6000_09"
 })
 
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("SELECT * FROM User;", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
+// checks if connected successfully if not returns error message
+con.connect((err) => {
+    if (err) {
+        console.error("DB connection failed: " + err.stack);
+        return;
+    }
+    console.log('Connected to DB successfully');
+
+
+    // execute query once connection is established.
+    con.query("SELECT * FROM User;", (err, result, fields) => {
+        if (err) {
+            console.error("Error when executing query", err);
+            return;
+        }
+
+        console.log('Query: ', result);
+
     });
+
 });
+
+
+// old code for now
+
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("SELECT * FROM User;", function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// });
