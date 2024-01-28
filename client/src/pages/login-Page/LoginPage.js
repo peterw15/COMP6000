cf1149d2256b1b4f53a2f4420eb18418ec164ed8
Bradley <94051876+Bm472@users.noreply.github.com>
2024-01-28 21:39:14 +0000

    Complete redesign of login page with new branding

--- client/src/pages/login-Page/LoginPage.js
+++ client/src/pages/login-Page/LoginPage.js
@@ -9,7 +9,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Image from 'react-bootstrap/Image';
-import library from './library.jpg';
+import background from './background.png';
+import logo from './logoAbove.png';
 
 
 
@@ -28,28 +29,29 @@ function LoginPage() {
 
     return (
         <>  
-            <Container fluid style={{height:"100vh"}}>
-                <Row style={{width:"100%"}}>
-                    <Col style={{width:"50%", height: "100vh", backgroundImage: `url(${library})`, backgroundSize : "cover", backgroundPosition:"center", borderRight: "solid 5px", borderColor: "#1e1e1e"}}>
+            <Container fluid style={{height:"100vh", backgroundImage: `url(${background})`, backgroundSize : "cover", backgroundPosition:"center"}} className="text-center">
+                <Row style={{width:"100%"}} className="text-center">
+                    <Col style={{width:"33.33%", height: "100vh"}}>
                     </Col>
-                    <Col style={{width:"50%", backgroundColor: "#252526"}}>
-                        <Row style={{height:"20%"}}></Row>
-                        <Row style={{height:"33%"}}>
-                            <Container fluid>
+                    <Col style={{width:"33.33%", backgroundColor: "#202020", marginTop:"50px", marginBottom:"50px", marginLeft:"20px"}} className="text-center" >
+                        <Row style={{height:"15%"}}>
+                        </Row>
+                        <Row style={{height:"33%"}} className="text-center">
+                            <img src={logo} ></img>
+                            <Container fluid style={{backgroundColor: "#202020"}}>
                                 <Row>
                                     <Col></Col>
                                     <Col className="text-center">
-                                        <div className="header" style={{color: "#3299ff"}}>Sign In</div>
                                     </Col>
                                     <Col></Col>
                                 </Row>
                                 <Row style={{height:"10%"}}></Row>
                                 <Row>
-                                    <Col></Col>
-                                    <Col className="text-center">
+                                    <Col style={{width:"33.33%"}}></Col>
+                                    <Col className="text-center" style={{width:"33.33%", backgroundColor: "#202020"}}>
                                         <Form.Control type="text" id="username" placeholder="Username"/>
                                     </Col>
-                                    <Col></Col>
+                                    <Col style={{width:"33.33%"}}></Col>
                                 </Row>
                                 <Row style={{height:"5%"}}></Row>
                                 <Row>
@@ -63,19 +65,19 @@ function LoginPage() {
                                 <Row>
                                     <Col style={{width:"33%"}}></Col>
                                     <Col style={{width:"33%"}} className="text-center">
-                                        <Button variant="primary" id="submit" onClick={handleClick} style={{height: "45px", width:"30%"}}>Submit</Button>
+                                        <Button variant="outline-primary" id="submit" className="btn" onClick={handleClick} style={{height: "45px", width:"40%", backgroundColor:"#252526", color:"#ffffff", fontSize:"90%", borderColor :"#18cdc6"}}>Login</Button>
                                     </Col>
                                     <Col style={{width:"33%"}}></Col>
                                 </Row>
                                 <Row style={{height:"1%"}}></Row>
-                                <Row>
+                                <Row className="text-center">
                                     <Col></Col>
                                     <Col className="text-center">
-                                        <Button variant="primary" id="forgotPassword" className="btn" style={{height: "45px", width:"40%", marginLeft:"50%", fontSize:"80%"}}>Forgot Login</Button>
+                                        <Button variant="outline-primary" id="forgotPassword" className="btn" style={{height: "45px", width:"40%", marginLeft:"50%", fontSize:"70%", backgroundColor: "#252526", color:"#ffffff", borderColor :"#18cdc6"}}>Forgot Login</Button>
                                     </Col>
                                     <Col className="text-center">
                                         <Link to="/register" >
-                                                <Button variant="primary" id="createAccount" className="btn" style={{height: "45px", width:"40%", marginRight:"50%", fontSize:"80%"}}>Create Account</Button>
+                                                <Button variant="outline-primary" id="createAccount" className="btn" style={{height: "45px", width:"40%", marginRight:"50%", fontSize:"70%", backgroundColor: "#252526", color:"#ffffff", borderColor :"#18cdc6"}}>Sign Up</Button>
                                         </Link>
                                     </Col>
                                     <Col >
@@ -86,8 +88,9 @@ function LoginPage() {
                                 </Container>
                             </Container>
                         </Row>
-                        <Row style={{height:"33%"}}></Row>
+                        <Row style={{height:"33.33%"}}></Row>
                     </Col>
+                    <Col style={{width:"33.33%", height: "100vh"}}></Col>
                 </Row>
             </Container>
         </>
