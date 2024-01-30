a7719a759ef97a15f289e1ecd74e8557b07b8c94
Bradley <94051876+Bm472@users.noreply.github.com>
2024-01-30 01:08:36 +0000

    Continued designing Create Event Page. Updated title and favicon

--- client/src/pages/createEvent-page/CreateEventPage.js
+++ client/src/pages/createEvent-page/CreateEventPage.js
@@ -10,6 +10,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
+import { ListGroup } from 'react-bootstrap';
 
 
 function CreateEventPage() {
@@ -129,13 +130,31 @@ function CreateEventPage() {
         ref.current.prev();
     }
 
+    const [eventName,setEventName] = useState("");
+    const [eventDateTime,setEventDateTime] = useState("");
+    const [eventLocation,setEventLocation] = useState("");
+    const [eventDescription,setEventDescription] = useState("");
+    const [eventPrice,setEventPrice] = useState("");
+    const [eventTags,setEventTags] = useState([]);
+
+    function summaryFunction() {
+        setEventName(document.getElementById("eventName").value);
+        setEventDateTime(document.getElementById("eventDateTime").value);
+        setEventLocation(document.getElementById("location").value);
+        setEventDescription(document.getElementById("description").value);
+        setEventPrice(document.getElementById("price").value);
+        setEventTags(gatherTags().map((tag) => tag + " | "));
+        onNext();
+    }
+
+ 
 
     return (
             <>
                 <HeaderBar></HeaderBar>
                 <Container fluid style={{height:"100vh", width: "100%", backgroundColor:"#202020", margin:"0"}}>
                         <Row style={{width:"100%",height: "100%", margin:"0"}} className="text-center">
-                                <Carousel style={{marginTop: "100px", height:"70%", width:"70%", marginLeft:"15%", marginRight: "15%", borderColor: "#18cdc6", border: "3px solid #18cdc6"}} ref={ref} interval={null} controls={true}>
+                                <Carousel style={{marginTop: "100px", height:"70%", width:"70%", marginLeft:"15%", marginRight: "15%", borderColor: "#18cdc6", border: "3px solid #18cdc6"}} className="text-center" ref={ref} interval={null} controls={false}>
                                     <Carousel.Item>
                                         <Container fluid style={{marginTop: "10%", marginBottom :"5%"}}>
                                             <h2 className="subHeader" style={{color:"#18cdc6", fontSize:"70px"}}>Let's create an event!</h2>
@@ -143,33 +162,49 @@ function CreateEventPage() {
                                         </Container>
                                     </Carousel.Item>
                                     <Carousel.Item>
-                                        <Container fluid style={{marginTop: "10%", marginBottom :"5%"}}>
-                                            <h2 className="subHeader" style={{color:"#18cdc6"}}>What would you like your event to be called?</h2>
-                                            <input id="eventName" style={{marginTop:"5%"}} />
+                                        <Container fluid style={{marginTop: "10%", marginBottom :"5%"}} className="text-center">
+                                            <Row className="text-center" styke={{height:"100%"}}>
+                                                <Col style={{width:"33%"}}></Col>
+                                                <Col style={{width:"33%"}}>
+                                                    <h2 className="subHeader" style={{color:"#18cdc6"}}>What would you like your event to be called?</h2>
+                                                    <Form.Control type="text" id="eventName" style={{width:"50%"}} className="text-center"/>
+                                                    <Button variant="outline-primary" id="namePrev" className="btn" onClick={onPrev}>Back</Button>
+                                                    <Button variant="outline-primary" id="nameNext" className="btn" onClick={onNext}>Next</Button>
+                                                </Col>
+                                                <Col style={{width:"33%"}}></Col>
+                                            </Row>
                                         </Container>
                                     </Carousel.Item>
                                     <Carousel.Item>
                                         <Container fluid style={{marginTop: "10%", marginBottom :"5%"}}>
                                             <h2 className="subHeader" style={{color:"#18cdc6"}}>What is the date/time of your event?</h2>
                                             <input id="eventDateTime" style={{marginTop:"5%"}} />
+                                            <Button variant="outline-primary" id="dateTimePrev" className="btn" onClick={onPrev}>Back</Button>
+                                            <Button variant="outline-primary" id="dateTimeNext" className="btn" onClick={onNext}>Next</Button>
                                         </Container>
                                     </Carousel.Item>
                                     <Carousel.Item>
                                         <Container fluid style={{marginTop: "10%", marginBottom :"5%"}}>
                                             <h2 className="subHeader" style={{color:"#18cdc6"}}>Where will your event be located?</h2>
                                             <input id="location" style={{marginTop:"5%"}}/>
+                                            <Button variant="outline-primary" id="locationPrev" className="btn" onClick={onPrev}>Back</Button>
+                                            <Button variant="outline-primary" id="locationNext" className="btn" onClick={onNext}>Next</Button>
                                         </Container>
                                     </Carousel.Item>
                                     <Carousel.Item>
                                         <Container fluid style={{marginTop: "10%", marginBottom :"5%"}}>
                                             <h2 className="subHeader" style={{color:"#18cdc6",}}>How would you describe your event?</h2>
                                             <input id="description" style={{marginTop:"5%"}} />
+                                            <Button variant="outline-primary" id="descriptionPrev" className="btn" onClick={onPrev}>Back</Button>
+                                            <Button variant="outline-primary" id="descriptionNext" className="btn" onClick={onNext}>Next</Button>
                                         </Container>
                                     </Carousel.Item>
                                     <Carousel.Item>
                                         <Container fluid style={{marginTop: "10%", marginBottom :"5%"}}>
                                             <h2 className="subHeader" style={{color:"#18cdc6"}}>How much will your event cost?</h2>
-                                            <input id="price" style={{marginTop:"5%"}} /> 
+                                            <input id="price" style={{marginTop:"5%"}} />
+                                            <Button variant="outline-primary" id="pricePrev" className="btn" onClick={onPrev}>Back</Button>
+                                            <Button variant="outline-primary" id="priceNext" className="btn" onClick={onNext}>Next</Button>
                                         </Container>
                                     </Carousel.Item>
                                     <Carousel.Item>
@@ -193,14 +228,29 @@ function CreateEventPage() {
                                                                 </Row>
                                                             ))
                                                         }
+                                                        <Button variant="outline-primary" id="tagsPrev" className="btn" onClick={onPrev}>Back</Button>
+                                                        <Button variant="outline-primary" id="tagsNext" className="btn" onClick={summaryFunction}>Next</Button>
                                                     </Col>
                                                     <Col></Col>
                                                 </Row>
                                             </Container>
                                     </Carousel.Item>
                                     <Carousel.Item>
-                                        <Container fluid style={{marginTop: "5%", marginBottom: "5%"}}> 
-                                            <Button variant="outline-primary" id="submit" className="btn" onClick={createEvent}>Submit</Button>
+                                        <Container fluid style={{marginTop: "5%", marginBottom: "5%"}}>
+                                            <Col></Col>
+                                            <Col>
+                                                <h2>Summary</h2>
+                                                <ListGroup style={{width:"50%"}}>
+                                                    <ListGroup.Item>Name: {eventName}</ListGroup.Item>
+                                                    <ListGroup.Item>Date/Time: {eventDateTime}</ListGroup.Item>
+                                                    <ListGroup.Item>Location: {eventLocation}</ListGroup.Item>
+                                                    <ListGroup.Item>Description: {eventDescription}</ListGroup.Item>
+                                                    <ListGroup.Item>Price: {eventPrice}</ListGroup.Item>
+                                                    <ListGroup.Item>Tags: {eventTags}</ListGroup.Item>
+                                                </ListGroup>
+                                                <Button variant="outline-primary" id="submit" className="btn" onClick={createEvent}>Create Event</Button>
+                                            </Col>
+                                            <Col></Col>
                                         </Container>
                                     </Carousel.Item>
                                 </Carousel>                             
