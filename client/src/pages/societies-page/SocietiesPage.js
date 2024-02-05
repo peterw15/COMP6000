8e835673c691df8a7f9dea1d3389dcc668315838
Luke <lukeelliottkent@gmail.com>
2024-02-05 15:20:48 +0000

    login check

--- client/src/pages/societies-page/SocietiesPage.js
+++ client/src/pages/societies-page/SocietiesPage.js
@@ -1,12 +1,15 @@
 // imports
-import React, { useState } from 'react';
-import './societiesPagesStylesheet.css';
-import ImgButton from './components/images/logoIcon.png';
-import HeaderBar from '../general-components/HeaderBar/HeaderBar.js';
+import React, { useEffect, useState } from 'react'; // react/states
+import './societiesPagesStylesheet.css'; // css access
+import ImgButton from './components/images/logoIcon.png'; // logo as button
+import HeaderBar from '../general-components/HeaderBar/HeaderBar.js'; // navbar
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; // for navigate
 
 function SocietiesPage() {
     // ripple animation
     const [ripple, setRipple] = useState(false);
+    const navigate = useNavigate();
 
     // when button is clicked
     const buttonClick = () => {
@@ -15,6 +18,23 @@ function SocietiesPage() {
             setRipple(false);
         }, 1000);
     };
+
+    // check user is logged in first
+    useEffect(() => {
+        axios.get('http://localhost:3001/loggedIn', {})
+            .then(res => {
+                console.log(res);
+                if (res.data == null) {
+                    navigate("/login");
+                } else {
+                    // societies stuff
+                }
+            })
+            .catch(error => {
+                console.error('Error checking loggedIn status:', error);
+            });
+    }, []);
+    
 
     return (
         <>
